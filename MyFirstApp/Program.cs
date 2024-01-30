ab7accb6fde2d1f7750fcc331facbaefbd692a63
Araudsepp <anriraudsepp@gmail.com>
2024-01-30 18:15:14 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//ralemdus küsib nime sisestamise
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+//Esimene voimalus
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+//String interpolatsion 2 voimalus, dollar kohustuslik
+Console.WriteLine($"Hello, {userName}!"); //Output
+
